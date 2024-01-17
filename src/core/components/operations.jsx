27890208a715d99c0357ca3e46dea3dd562f145a
Sergey Shehovcov <s.shehovcov@gk-adept.ru>
2024-01-17 09:50:28 +0300

    feat: virtuoso

--- src/core/components/operations.jsx
+++ src/core/components/operations.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import Im from "immutable"
+import {Virtuoso} from "react-virtuoso"
 
 export default class Operations extends React.Component {
 
@@ -21,9 +22,17 @@ export default class Operations extends React.Component {
   render() {
     let {
       specSelectors,
+      getComponent,
+      oas3Selectors,
+      layoutSelectors,
+      layoutActions,
+      getConfigs,
     } = this.props
 
     const taggedOps = specSelectors.taggedOperations()
+    const itemsInArray = Array.from(taggedOps.entries())
+    const validOperationMethods = specSelectors.validOperationMethods()
+    const OperationContainer = getComponent("OperationContainer", true)
 
     if(taggedOps.size === 0) {
       return <h3> No operations defined in spec!</h3>
@@ -31,63 +40,58 @@ export default class Operations extends React.Component {
 
     return (
       <div>
-        { taggedOps.map(this.renderOperationTag).toArray() }
+        <Virtuoso
+          style={{ height: "500px" }}
+          totalCount={taggedOps?.size}
+          data={itemsInArray}
+          // eslint-disable-next-line react/jsx-no-bind
+          itemContent={(index) => {
+            const tag = itemsInArray[index][0]
+            const tagObj = itemsInArray[index][1]
+            const OperationTag = getComponent("OperationTag")
+            const operations = tagObj.get("operations")
+
+            return <OperationTag
+              key={"operation-" + tag}
+              tagObj={tagObj}
+              tag={tag}
+              oas3Selectors={oas3Selectors}
+              layoutSelectors={layoutSelectors}
+              layoutActions={layoutActions}
+              getConfigs={getConfigs}
+              getComponent={getComponent}
+              specUrl={specSelectors.url()}>
+              <div className="operation-tag-content">
+                {
+                  operations.map(op => {
+                    const path = op.get("path")
+                    const method = op.get("method")
+                    const specPath = Im.List(["paths", path, method])
+
+                    if (validOperationMethods.indexOf(method) === -1) {
+                      return null
+                    }
+
+                    return (
+                      <OperationContainer
+                        key={`${path}-${method}`}
+                        specPath={specPath}
+                        op={op}
+                        path={path}
+                        method={method}
+                        tag={tag} />
+                    )
+                  }).toArray()
+                }
+              </div>
+            </OperationTag>
+
+          }}
+        />
         { taggedOps.size < 1 ? <h3> No operations defined in spec! </h3> : null }
       </div>
     )
   }
-
-  renderOperationTag = (tagObj, tag) => {
-    const {
-      specSelectors,
-      getComponent,
-      oas3Selectors,
-      layoutSelectors,
-      layoutActions,
-      getConfigs,
-    } = this.props
-    const validOperationMethods = specSelectors.validOperationMethods()
-    const OperationContainer = getComponent("OperationContainer", true)
-    const OperationTag = getComponent("OperationTag")
-    const operations = tagObj.get("operations")
-    return (
-      <OperationTag
-        key={"operation-" + tag}
-        tagObj={tagObj}
-        tag={tag}
-        oas3Selectors={oas3Selectors}
-        layoutSelectors={layoutSelectors}
-        layoutActions={layoutActions}
-        getConfigs={getConfigs}
-        getComponent={getComponent}
-        specUrl={specSelectors.url()}>
-        <div className="operation-tag-content">
-          {
-            operations.map(op => {
-              const path = op.get("path")
-              const method = op.get("method")
-              const specPath = Im.List(["paths", path, method])
-
-              if (validOperationMethods.indexOf(method) === -1) {
-                return null
-              }
-
-              return (
-                <OperationContainer
-                  key={`${path}-${method}`}
-                  specPath={specPath}
-                  op={op}
-                  path={path}
-                  method={method}
-                  tag={tag} />
-              )
-            }).toArray()
-          }
-        </div>
-      </OperationTag>
-    )
-  }
-
 }
 
 Operations.propTypes = {
